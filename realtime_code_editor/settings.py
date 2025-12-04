1db9a4b1df44e4d64ad88dedab4b398d940d4fd8
torjo <faysalhassantorjo8@gmail.com>
2025-12-04 22:44:17 +0600

    feat: Add PostgreSQL database service, initial Django project settings, and a login page UI.

--- realtime_code_editor/settings.py
+++ realtime_code_editor/settings.py
@@ -84,13 +84,24 @@ ASGI_APPLICATION = "realtime_code_editor.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'codematedb',
+        'USER': 'codemate',
+        'PASSWORD': 'codemate1234',
+        'HOST': 'postgres',
+        'PORT': '5432',
     }
 }
-
 
 CHANNEL_LAYERS = {
     "default": {

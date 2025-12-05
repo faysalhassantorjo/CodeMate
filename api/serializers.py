e76503942e4a0c37220063f4ebd3cc6fbdd6cdc8
torjo <faysalhassantorjo8@gmail.com>
2025-12-05 15:23:40 +0600

    feat: Implement DRF API for channel management, including listing, joining, and retrieving channel details.

--- api/serializers.py
+++ api/serializers.py
@@ -1,59 +1,59 @@
-# from editor.models import Channel, UserProfile
-# from rest_framework import generics
-# from rest_framework import serializers
+from editor.models import Channel, UserProfile
+from rest_framework import generics
+from rest_framework import serializers
 
 
-# class UserProfileSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = UserProfile
-#         fields = ['id', 'profile_picture']
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ['id', 'profile_picture']
 
-# class ChannelSerializer(serializers.ModelSerializer):
-#     participants = serializers.SerializerMethodField()
+class ChannelSerializer(serializers.ModelSerializer):
+    participants = serializers.SerializerMethodField()
     
-#     created_by = UserProfileSerializer(read_only=True)
+    created_by = UserProfileSerializer(read_only=True)
 
-#     class Meta:
-#         model = Channel
-#         fields = ['id', 'name', 'channel_picture', 'programing_language',
-#                   'created_by', 'participants', 'created_at', 'updated_at']
+    class Meta:
+        model = Channel
+        fields = ['id', 'name', 'channel_picture', 'programing_language',
+                  'created_by', 'participants', 'created_at', 'updated_at']
 
-#     def get_participants(self, obj):
-#         # Limit to first 5 participants
-#         participants = obj.participants.all()[:5]
+    def get_participants(self, obj):
+        # Limit to first 5 participants
+        participants = obj.participants.all()[:5]
         
-#         return UserProfileSerializer(participants, many=True).data
+        return UserProfileSerializer(participants, many=True).data
 
-# class JoinChannelSerializer(serializers.ModelSerializer):
-#     channel_id = serializers.CharField(write_only=True)
-#     class Meta:
-#         model = Channel
-#         fields = ['id', 'channel_id']
+class JoinChannelSerializer(serializers.ModelSerializer):
+    channel_id = serializers.CharField(write_only=True)
+    class Meta:
+        model = Channel
+        fields = ['id', 'channel_id']
         
 
-# from editor.models import CodeFile
-# class CodeFileSerializer(serializers.ModelSerializer):
-#     user = UserProfileSerializer(read_only=True)
-#     class Meta:
-#         model = CodeFile
-#         fields = '__all__'
+from editor.models import CodeFile
+class CodeFileSerializer(serializers.ModelSerializer):
+    user = UserProfileSerializer(read_only=True)
+    class Meta:
+        model = CodeFile
+        fields = '__all__'
 
-# class ChannelDetailsSerializer(serializers.ModelSerializer):
-#     channel_details = serializers.SerializerMethodField(read_only=True)
-#     channels_files = serializers.SerializerMethodField(read_only=True)
-#     class Meta:
-#         model = Channel
-#         fields = [
-#             'channel_details',
-#             'channels_files'
-#         ]
+class ChannelDetailsSerializer(serializers.ModelSerializer):
+    channel_details = serializers.SerializerMethodField(read_only=True)
+    channels_files = serializers.SerializerMethodField(read_only=True)
+    class Meta:
+        model = Channel
+        fields = [
+            'channel_details',
+            'channels_files'
+        ]
         
-#     def get_channel_details(self, obj):
+    def get_channel_details(self, obj):
         
-#         return ChannelSerializer(obj).data
+        return ChannelSerializer(obj).data
     
-#     def get_channels_files(self, obj):
-#         codefiles = CodeFile.objects.filter(channel=obj)
-#         if not codefiles:
-#             return {"detail":"No files found in this channel"}
-#         return CodeFileSerializer(codefiles, many=True).data
+    def get_channels_files(self, obj):
+        codefiles = CodeFile.objects.filter(channel=obj)
+        if not codefiles:
+            return {"detail":"No files found in this channel"}
+        return CodeFileSerializer(codefiles, many=True).data
--- api/views.py
+++ api/views.py
@@ -1,61 +1,61 @@
-# from django.shortcuts import render
-# from editor.models import Channel, UserProfile, CodeFile
-# from .serializers import ChannelSerializer,JoinChannelSerializer,ChannelDetailsSerializer
-# # Create your views here.
-# from rest_framework import generics,permissions
-# from rest_framework.response import Response
-# from django.db.models import Prefetch
+from django.shortcuts import render
+from editor.models import Channel, UserProfile, CodeFile
+from .serializers import ChannelSerializer,JoinChannelSerializer,ChannelDetailsSerializer
+# Create your views here.
+from rest_framework import generics,permissions
+from rest_framework.response import Response
+from django.db.models import Prefetch
 
-# # get object or get 404
-# from django.shortcuts import get_object_or_404
+# get object or get 404
+from django.shortcuts import get_object_or_404
 
-# class ChannelListAPIView(generics.ListAPIView):
-#     serializer_class = ChannelSerializer
-#     permission_classes =[permissions.IsAuthenticated]
+class ChannelListAPIView(generics.ListAPIView):
+    serializer_class = ChannelSerializer
+    permission_classes =[permissions.IsAuthenticated]
     
     
-#     def get_queryset(self):
-#         user = self.request.user
-#         # Adjust depending on your model relationship
-#         return(
-#             Channel.objects.filter(participants__user=user)
-#             .select_related('created_by')
-#             .prefetch_related('participants')
-#         )
+    def get_queryset(self):
+        user = self.request.user
+        # Adjust depending on your model relationship
+        return(
+            Channel.objects.filter(participants__user=user)
+            .select_related('created_by')
+            .prefetch_related('participants')
+        )
     
-# class JoinChannelAPIView(generics.GenericAPIView):
-#     serializer_class = JoinChannelSerializer
-#     permission_classes =[permissions.IsAuthenticated]
+class JoinChannelAPIView(generics.GenericAPIView):
+    serializer_class = JoinChannelSerializer
+    permission_classes =[permissions.IsAuthenticated]
     
-#     def post(self, request):
-#         print(request.data)
-#         pk = request.data.get('channel_id')
-#         user = request.user
-#         try:
-#             channel = Channel.objects.get(pk=pk)
-#         except Channel.DoesNotExist:
-#             return Response({"detail": "Channel not found."}, status=404)
+    def post(self, request):
+        print(request.data)
+        pk = request.data.get('channel_id')
+        user = request.user
+        try:
+            channel = Channel.objects.get(pk=pk)
+        except Channel.DoesNotExist:
+            return Response({"detail": "Channel not found."}, status=404)
         
-#         if channel.participants.filter(user=user).exists():
-#             return Response({"detail": "User already in channel."}, status=400)
+        if channel.participants.filter(user=user).exists():
+            return Response({"detail": "User already in channel."}, status=400)
         
-#         user_profile = get_object_or_404(UserProfile, user=user)
-#         channel.participants.add(user_profile)
-#         channel.save()
+        user_profile = get_object_or_404(UserProfile, user=user)
+        channel.participants.add(user_profile)
+        channel.save()
         
-#         serializer = self.get_serializer(channel)
-#         return Response(serializer.data)
+        serializer = self.get_serializer(channel)
+        return Response(serializer.data)
     
-#     def get(self, reqeust):
-#         user = reqeust.user
-#         userprofile = get_object_or_404(UserProfile, user=user)
+    def get(self, reqeust):
+        user = reqeust.user
+        userprofile = get_object_or_404(UserProfile, user=user)
         
-#         return Response({f"{user.username} joined in": Channel.objects.filter(participants=userprofile).count()})
+        return Response({f"{user.username} joined in": Channel.objects.filter(participants=userprofile).count()})
     
 
 
-# class ChannelDetailAPIView(generics.RetrieveAPIView):
-#     serializer_class = ChannelDetailsSerializer
-#     permission_classes =[permissions.IsAuthenticated]
-#     queryset = Channel.objects.all()
-#     lookup_field = 'pk'
+class ChannelDetailAPIView(generics.RetrieveAPIView):
+    serializer_class = ChannelDetailsSerializer
+    permission_classes =[permissions.IsAuthenticated]
+    queryset = Channel.objects.all()
+    lookup_field = 'pk'
